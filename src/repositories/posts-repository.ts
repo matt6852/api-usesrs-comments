b8e57cbf3cb16e2ed58798857e035ddc41f9da1f
Сергей Мячин <s.myachin@Sergey-MacBook-Air.local>
2022-05-09 20:30:50 +0300

    make it better

--- src/repositories/posts-repository.ts
+++ src/repositories/posts-repository.ts
@@ -8,25 +8,27 @@ type newBlogger = {
 export const postsRepository = {
   async getAllPosts(title: any) {
     const filter: any = {};
-    if (title) {
-      filter.title = { $regex: title };
-    }
-    const allPosts = await postsCollection.find(filter).toArray();
-    const redone = allPosts.map((post: any) => {
-      const newOne: any = {
-        id: post.id,
-        title: post.title,
-        shortDescription: post.shortDescription,
-        content: post.content,
-        bloggerId: post.bloggerId,
-        bloggerName: post.bloggerName,
-      };
-      return newOne;
-    });
-    return redone;
+
+    const allPosts = await postsCollection
+      .find({}, { projection: { _id: 0 } })
+      .toArray();
+    // const redone = allPosts.map((post: any) => {
+    //   const newOne: any = {
+    //     id: post.id,
+    //     title: post.title,
+    //     shortDescription: post.shortDescription,
+    //     content: post.content,
+    //     bloggerId: post.bloggerId,
+    //     bloggerName: post.bloggerName,
+    //   };
+    //   return newOne;
+    // });
+    return allPosts;
   },
   async createNewPost(newPost: any) {
-    const done = await postsCollection.insertOne(newPost);
+    const done = await postsCollection.insertOne(newPost, {
+      forceServerObjectId: true,
+    });
     return done.acknowledged;
   },
   async deleteBlogger(id: number) {
@@ -48,10 +50,11 @@ export const postsRepository = {
     return createdOne;
   },
   async updatedSinglePost(id: number, updatedOne: object) {
-    const isUpdated = await postsCollection.updateOne(
+    const isUpdated = await postsCollection.findOneAndUpdate(
       { id },
-      { $set: { ...updatedOne } }
+      { $set: { ...updatedOne } },
+      { upsert: true }
     );
-    return isUpdated.modifiedCount >= 1;
+    return isUpdated.value;
   },
 };
